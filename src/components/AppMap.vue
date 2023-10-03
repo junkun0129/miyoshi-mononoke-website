07f9b339182816c738b4be756d277a6dd794aece
unknown <penpenetai@icloud.com>
2023-10-03 00:50:37 -0700

    added a map and schedule

--- src/components/AppMap.vue
+++ src/components/AppMap.vue
@@ -1,27 +1,24 @@
 <template>
   <div>アクセス</div>
-  <GoogleMap
-    api-key="AIzaSyCLMvo_s8JFciYhn2XIBjYADYPn6Vzk_Yw"
-    style="width: 100%; height: 100px"
-  >
-    <Marker :options="markerOptions"></Marker>
-  </GoogleMap>
+  <div id="mapContainer"></div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import { GoogleMap, Marker } from "vue3-google-map";
+import { defineComponent, onMounted } from "vue";
+import mapboxgl from "mapbox-gl";
+mapboxgl.accessToken =
+  "pk.eyJ1IjoianVtcGVpNTUyNCIsImEiOiJjbG45bXdlaXgwODFsMnJuejY2Y21xNHlmIn0.4SGTIQRidawCpYXgzUxXlQ";
 export default defineComponent({
   name: "AppMap",
-  components: { GoogleMap, Marker },
   setup() {
-    const center = { lat: 40.689247, lng: -74.044502 };
-    const markerOptions = {
-      position: center,
-      label: "L",
-      title: "LADY LIBERTY",
-    };
-    return { center, markerOptions };
+    onMounted(() => {
+      const map = new mapboxgl.Map({
+        container: "mapContainer",
+        style: "mapbox://styles/mapbox/streets-v12",
+        center: [-71.224518, 42.213995],
+        zoom: 9,
+      });
+    });
   },
 });
 </script>
